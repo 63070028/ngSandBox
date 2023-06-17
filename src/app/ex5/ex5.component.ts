1e9510c77cc5e94c477ce423104a92028f2b154a
63070028 <63070028@kmitl.ac.th>
2023-06-17 15:10:14 +0700

    ex5 ViewChild & ViewChildren

--- src/app/ex5/ex5.component.ts
+++ src/app/ex5/ex5.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-ex5',
   templateUrl: './ex5.component.html',
   styleUrls: ['./ex5.component.scss']
 })
-export class Ex5Component {
+export class Ex5Component{
+  boxs:number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+  
+  @ViewChild("boxElement") boxElement?:ElementRef<HTMLDivElement>;
+  
+  @ViewChildren("boxElements") boxElements!:QueryList<ElementRef>;
+  @ViewChildren("boxToggleElements") boxToggleElements!:QueryList<ElementRef>;
+  
+  ngOnInit(){
+      console.log(this.boxElement)
+  }
+
+  ngAfterViewInit(){
+    if(this.boxElement != undefined){
+      console.log(this.boxElement.nativeElement)
+    }
+    
+    this.boxElements.forEach(e => {console.log(e.nativeElement)})
+  }
+
+  changeBgToggle(e:HTMLDivElement){
+    if(e.classList[0] === 'bg-warning'){
+      e.classList.replace('bg-warning', 'bg-danger')
+    }
+    else{
+      e.classList.replace('bg-danger', 'bg-warning')
+    }
+  }
+
+  changeBgSelect(taget:HTMLDivElement){
+    this.boxToggleElements.map(box => box.nativeElement.classList.replace('bg-danger', 'bg-warning'))
+    taget.classList.replace('bg-warning', 'bg-danger')
+  }
+
 
 }
